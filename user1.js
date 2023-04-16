a2bcdcde2e662ab6b2e491e2bba9c929065f2328
rendrarya <rendrarya12345@gmail.com>
2023-04-16 14:33:08 +0800

    first commit

---
+++ user1.js
@@ -0,0 +1,21 @@
+let net = require("net")
+let readline = require("readline")
+
+
+const client = new net.Socket()
+client.connect(0812, process.argv[2] ?? "localhost", () => {
+  console.log("Connected to server")
+})
+client.on("data", (data) => {
+  console.log(data.toString("utf-8"))
+})
+
+const reader = readline.createInterface({ input: process.stdin })
+reader.on("line", (line) => {
+  if (line.startsWith("@")) {
+    const [recipient, messageContent] = line.slice(1).split(" ")
+    client.write(`@${recipient} ${messageContent}\n`)
+  } else {
+    client.write(`${line}\n`)
+  }
+})
